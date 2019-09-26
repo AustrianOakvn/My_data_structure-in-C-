11d0bc57091fce5e934d3f1bb330e9fad199e6a2
AustrianOakvn <46952800+AustrianOakvn@users.noreply.github.com>
2019-09-26 19:40:24 +0700

    add BinaryTree folder

---
+++ BinaryTree/Binary_Tree_Implication.cpp
@@ -0,0 +1,24 @@
+﻿// Binary_Tree_Implication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include"Node.h"
+using namespace std;
+
+int main()
+{
+	btree mytree;
+	mytree.insert(50);
+	mytree.insert(30);
+	mytree.insert(20);
+	mytree.insert(40);
+	mytree.insert(70);
+	mytree.insert(60);
+	mytree.insert(80);
+	mytree.inorder_print();
+	mytree.search(10);
+	cout << mytree.search(10) << endl;
+    std::cout << "Hello World!\n"; 
+}
+
